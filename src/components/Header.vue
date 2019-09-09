eb01cbc893da8f9225b8b1ed97bbd950d8d781d6
taodtu <taodtu.gmail.com>
2019-09-09 10:24:19 +0100

    implement the header component

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <header>
+   <img src="../assets/034-microphone.svg" alt="title" className="logo" id="logo" />  
+    <h1>Nc News <span role="img" aria-label="say no evil">🙊️</span></h1>
+  </header>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+}
+</script>
+
+<style scoped>
+ h1 {
+     margin: 0.2em 0.5em;
+ }
+</style>
