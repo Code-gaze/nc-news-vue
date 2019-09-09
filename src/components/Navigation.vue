338b66c8c609f1be07f69c2bbd05d2af832ce464
taodtu <taodtu.gmail.com>
2019-09-09 13:22:21 +0100

    implement topics routes

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -2,7 +2,7 @@
     <nav >
         <router-link to='/' class="title-link">Home</router-link>
             <p v-if='loading'>...loading</p>
-            <router-link v-for='topic in topics' :key='topic.slug' :to="'/' + topic.slug" class="title-link">
+            <router-link v-for='topic in topics' :key='topic.slug' :to="'/topics/' + topic.slug" class="title-link">
                 {{topic.slug}}
             </router-link>
       </nav>
