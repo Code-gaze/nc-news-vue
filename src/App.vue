372e2d2bbbdb82d0b6867118a088a68f81227add
taodtu <taodtu.gmail.com>
2019-09-09 10:31:07 +0100

    Implement Footer component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
    <Header />
+   <Footer />
   </div>
 </template>
 
 <script>
-import Header from './components/Header'
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 export default {
   name: 'app',
   components: {
     Header,
+    Footer,
   }
 }
 </script>
