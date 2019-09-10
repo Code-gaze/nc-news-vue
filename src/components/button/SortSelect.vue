5027e7c7131ce2b1350393290985af138bbf358b
taodtu <taodtu.gmail.com>
2019-09-10 15:48:28 +0100

    create SortSelect component

---
+++ src/components/button/SortSelect.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="sort">
+   <label>SORT BY:</label>
+   <select :value='sortValue' @change="$emit('sortChange', $event.target.value)">
+    <option v-for='option in options' :key='option' :value='option'>{{option}}</option>
+   </select>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'SortSelect',
+    props:['sortValue', 'options']
+}
+</script>
