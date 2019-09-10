84f98d4e3eb3b3ff52cf96c5c3f47016810c5d69
taodtu <taodtu.gmail.com>
2019-09-10 13:39:05 +0100

    implement the userItem page

--- src/components/user/UserPage.vue
+++ src/components/user/UserPage.vue
@@ -1,10 +1,36 @@
 <template>
-    <div>in user page</div>
+    <div>
+        <UserItem :username='author.username' :name='author.name' :avatar_url='author.avatar_url'/>
+    </div>
 </template>
 
 <script>
+import UserItem from './UserItem';
+import { mapGetters } from 'vuex';
+import {getUser}  from '../api'
+
 export default {
-    name: 'UserPage'
+    name: 'UserPage',
+    components:{
+       UserItem,
+
+    },
+    props:{
+        user:{type: String}
+    },
+    computed:{
+        ...mapGetters([
+            'author',
+        ])
+    },
+    watch:{
+      user(){
+          this.$store.dispatch('getAuthor', this.user)
+      }
+    },
+    created(){
+         this.$store.dispatch('getAuthor', this.user)
+    }
 }
 </script>
 
--- src/store/modules/user/index.js
+++ src/store/modules/user/index.js
@@ -1,21 +1,32 @@
+import {getUser} from '../../../components/api';
+
 const state = {
     user: 'jessjelly',
+    author:{},
 };
 
 const mutations ={
     UPDATE_USER (state, payload){
         state.user = payload
+    },
+    GET_AUTHOR (state, payload){
+        state.author = payload
     }
 };
 
 const actions ={
     updateUser({commit}, payload){
        commit('UPDATE_USER', payload)
-    }
+    },
+    getAuthor({commit}, username){
+        getUser(username)
+         .then(author=>commit('GET_AUTHOR', author))
+     },
 };
 
 const getters = {
     user:state=>state.user,
+    author:state=>state.author,
 };
 
 const userModule = {
